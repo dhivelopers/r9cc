3c666a0cbf2bebc1fb4dd0b40f8d3e3129ffb7dd
ainnooo <aiai10piano@gmail.com>
2022-02-10 00:07:58 +0900

    STEP1: return int

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let ret_value = env::args().nth(1).unwrap();
+    println!(".intel_syntax noprefix");
+    println!(".global main");
+    println!("main:");
+    println!("\tmov rax, {}", ret_value);
+    println!("\tret");
 }
