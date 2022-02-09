a3403dcf650fa5215e447de38221a57a419c670d
ainnooo <aiai10piano@gmail.com>
2022-02-10 00:57:02 +0900

    improve unrwap() error handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::env;
+use std::process;
 
 fn main() {
-    let ret_value = env::args().nth(1).unwrap();
+    let ret_value = env::args().nth(1).unwrap_or_else(|| {
+        eprintln!("usage: ./r9cc <number>");
+        process::exit(1);
+    });
     println!(".intel_syntax noprefix");
     println!(".global main");
     println!("main:");
