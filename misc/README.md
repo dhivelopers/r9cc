73b96652c3b9c43fc52d9e19d6cd376557689e80
Masanori Tani <metro@uta8a.org>
2022-02-20 08:02:10 +0000

    Testcase add

--- misc/README.md
+++ misc/README.md
@@ -1,1 +1,5 @@
 - convert.py: convert assert testcase to file(use this at project top)
+
+```
+python3 misc/convert.py
+```
--- misc/convert.py
+++ misc/convert.py
@@ -1,7 +1,22 @@
 text = """
-assert 10 '-10+20'
-assert 10 '- -10'
-assert 10 '- - +10'
+assert 0 '0==1'
+assert 1 '42==42'
+assert 1 '0!=1'
+assert 0 '42!=42'
+
+assert 1 '0<1'
+assert 0 '1<1'
+assert 0 '2<1'
+assert 1 '0<=1'
+assert 1 '1<=1'
+assert 0 '2<=1'
+
+assert 1 '1>0'
+assert 0 '1>1'
+assert 0 '1>2'
+assert 1 '1>=0'
+assert 1 '1>=1'
+assert 0 '1>=2'
 """
 
 # use this at project top directory
